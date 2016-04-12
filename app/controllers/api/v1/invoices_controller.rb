09088d131e8fe9364a7158f2ac16520504d8d678
Julian Feliciano <julsfelic@gmail.com>
2016-04-12 12:00:17 -0600

    Update invoice find endpoint
    Added access to search for other attributes related to invoice

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -16,7 +16,7 @@ module Api
       private
 
       def invoice_params
-        params.permit(:id, :customer_id, :merchant_id, :status)
+        params.permit(:id, :customer_id, :merchant_id, :status, :created_at, :updated_at)
       end
     end
   end
