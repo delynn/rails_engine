ff4eec450973a6565892cc9d43f5e48c79421f15
Julian Feliciano <julsfelic@gmail.com>
2016-04-12 17:40:38 -0600

    Add endpoint for associated invoice items
    Added an API endpoint for invoice and it's associated invoice items

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -3,4 +3,5 @@ class InvoiceItem < ActiveRecord::Base
   validates :invoice_id, presence: true
   validates :quantity,   presence: true
   validates :unit_price, presence: true
+  belongs_to :invoice
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -54,6 +54,17 @@ FactoryGirl.define do
         create_list(:transaction, evaluator.transactions_count, invoice: invoice)
       end
     end
+
+    factory :invoice_with_invoice_items do
+
+      transient do
+        invoice_items_count 3
+      end
+
+      after(:create) do |invoice, evaluator|
+        create_list(:invoice_item, evaluator.invoice_items_count, invoice: invoice)
+      end
+    end
   end
 
   factory :invoice_item do
