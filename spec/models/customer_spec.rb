906958a6a88aaf4413b5e88ed6f10b66b336e4a5
Julian Feliciano <julsfelic@gmail.com>
2016-04-12 00:37:12 -0600

    Add endpoint for finding customer by attribute
    Added case insensitivity to finding a customer by it's attribute

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,3 +4,12 @@ RSpec.describe Customer, "validations" do
   it { is_expected.to validate_presence_of(:first_name) }
   it { is_expected.to validate_presence_of(:last_name) }
 end
+
+RSpec.describe Customer, type: :model do
+  it "downcases attributes when saved" do
+    customer = Customer.create(first_name: "JULIAN", last_name: "FELICIANO")
+
+    expect(customer.first_name).to eq("julian")
+    expect(customer.last_name).to  eq("feliciano")
+  end
+end
