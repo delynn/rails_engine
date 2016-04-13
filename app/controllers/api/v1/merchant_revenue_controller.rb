01ebbfae959fee013f3ce41d5478820530c55d7c
Julian Feliciano <julsfelic@gmail.com>
2016-04-13 14:35:21 -0600

    Add endpoint for returning total revenue by date
    Added an API endpoint for returning total revenue specific to date

--- app/controllers/api/v1/merchant_revenue_controller.rb
+++ app/controllers/api/v1/merchant_revenue_controller.rb
@@ -3,7 +3,12 @@ module Api
     class MerchantRevenueController < ApiController
       def show
         revenue = Hash.new
-        revenue[:revenue] = Invoice.revenue_by_merchant(params[:id])
+
+        if params[:date]
+          revenue[:revenue] = Invoice.revenue_by_merchant_date(params[:id], params[:date])
+        else
+          revenue[:revenue] = Invoice.revenue_by_merchant(params[:id])
+        end
         respond_with revenue
       end
     end
